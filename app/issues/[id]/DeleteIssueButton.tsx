d312d914ecb2e14a4f01c26a351dffd5c83e0e8b
Abdelali Benajaji <abdelalibenajaji@gmail.com>
2024-12-04 23:30:32 +0100

    add the dialog on delete

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,14 +1,31 @@
-import { Pencil2Icon, TrashIcon } from '@radix-ui/react-icons'
-import { Button } from '@radix-ui/themes'
+import { TrashIcon } from '@radix-ui/react-icons'
+import { AlertDialog, Button } from '@radix-ui/themes'
 import Link from 'next/link'
 import React from 'react'
 
 function DeleteIssueButton({issueId}: {issueId: number}) {
   return (
+    <AlertDialog.Root>
+    <AlertDialog.Trigger>
     <Button color='red'>
         <TrashIcon></TrashIcon>
-        <Link href={`/issues/${issueId}/edit`} >Delete issue</Link>
+        Delete issue
     </Button>
+    </AlertDialog.Trigger>
+    <AlertDialog.Content>
+      <AlertDialog.Title>Confirm Deletetion</AlertDialog.Title>
+      <AlertDialog.Description>Are you sure you want delete this Item?</AlertDialog.Description>
+      <div style={{ display: "flex", gap: 25, justifyContent: "flex-end" }}>
+					<AlertDialog.Cancel>
+						<Button color='violet'>Cancel</Button>
+					</AlertDialog.Cancel>
+					<AlertDialog.Action>
+						<Button color='red'>Yes, delete account</Button>
+					</AlertDialog.Action>
+				</div>
+    </AlertDialog.Content>
+
+  </AlertDialog.Root>
   )
 }
 
